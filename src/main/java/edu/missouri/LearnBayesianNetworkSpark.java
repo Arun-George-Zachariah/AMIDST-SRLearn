e95b483e9c66ba535a6a40355a6cf739d554309f
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-12-03 11:45:19 -0600

    Converting to a maven project.

---
+++ src/main/java/edu/missouri/LearnBayesianNetworkSpark.java
@@ -0,0 +1,69 @@
+package edu.missouri;
+
+import edu.missouri.constants.Constants;
+import edu.missouri.util.CSV2JSON;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.core.utils.DAGGenerator;
+import eu.amidst.sparklink.core.io.DataSparkLoader;
+import eu.amidst.sparklink.core.learning.ParallelMaximumLikelihood;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.sql.SQLContext;
+import eu.amidst.sparklink.core.data.DataSpark;
+
+
+public class LearnBayesianNetworkSpark {
+
+    public static void main(String[] args) throws Exception {
+        if(args.length != 1) {
+            System.out.println("edu.missouri.LearnBayesianNetworkSpark <INPUT>");
+            System.exit(-1);
+        }
+
+        // Obtaining the input file.
+        String input = args[0];
+
+        // If the input is a CSV, we convert it to as ARFF.
+        String[] fileSplits = input.split("\\.");
+        if(fileSplits[fileSplits.length-1].equals(Constants.CSV_EXTENSION)) {
+            input = CSV2JSON.getInstance().convertCSV2JSON(input);
+        }
+
+        // Validating the conversion.
+        if(input == null) {
+            System.out.println("LearnBayesianNetworkSpark :: main :: Invalid input provided.");
+            System.exit(-1);
+        }
+
+        // Creating the Spark context.
+        SparkConf conf = new SparkConf().setAppName(Constants.APP_NAME).setMaster(Constants.MASTER);
+        SparkContext sc = new SparkContext(conf);
+
+        // Creating the Spark SQL context.
+        SQLContext sqlContext = new SQLContext(sc);
+
+        // Opening the data stream.
+        DataSpark dataSpark = DataSparkLoader.open(sqlContext, input);
+
+        // Creating a ParallelMaximumLikelihood object.
+        ParallelMaximumLikelihood parameterLearningAlgorithm = new ParallelMaximumLikelihood();
+
+        // Fixing the DAG structure.
+        parameterLearningAlgorithm.setDAG(DAGGenerator.getNaiveBayesStructure(dataSpark.getAttributes(), "W"));
+
+        // Setting the batch size which will be employed to learn the model in parallel.
+        parameterLearningAlgorithm.setBatchSize(100);
+
+        // Setting the data to be used for leaning the parameters.
+        parameterLearningAlgorithm.setDataSpark(dataSpark);
+
+        // Performing the learning.
+        parameterLearningAlgorithm.runLearning();
+
+        // Obtaining the model.
+        BayesianNetwork bn = parameterLearningAlgorithm.getLearntBayesianNetwork();
+
+        System.out.println(bn);
+    }
+
+}
---
+++ src/main/java/edu/missouri/util/CSV2JSON.java
@@ -0,0 +1,62 @@
+package edu.missouri.util;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.csv.*;
+import edu.missouri.constants.Constants;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+
+public class CSV2JSON {
+    public static CSV2JSON instance = null;
+
+    private CSV2JSON() {
+
+    }
+
+    public static CSV2JSON getInstance() {
+        if(instance == null) {
+            instance = new CSV2JSON();
+        }
+        return instance;
+    }
+
+
+    public static String convertCSV2JSON(String csvInPath) throws Exception {
+        String[] splits = csvInPath.split("\\.");
+
+        // Validating the input.
+        if(!splits[splits.length-1].equals(Constants.CSV_EXTENSION)) {
+            System.out.println("CSV2JSON :: convertCSV2JSON :: Extension not ." + Constants.CSV_EXTENSION);
+            return null;
+        }
+
+        // Creating an out path based on the input.
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<splits.length - 1; i++) {
+            sb.append(splits[i]);
+        }
+        sb.append(Constants.DOT + Constants.JSON_EXTENSION);
+
+        try (FileWriter fileWriter = new FileWriter(new File(sb.toString()))) {
+            // Reading the contents of the CSV and converting it to a JSON.
+            CsvSchema csvSchema = CsvSchema.emptySchema().withHeader();
+            CsvMapper csvMapper = new CsvMapper();
+            MappingIterator<Map<?, ?>> mappingIterator =  csvMapper.reader().forType(Map.class).with(csvSchema).readValues(new File(csvInPath));
+            List<Map<?, ?>> list = mappingIterator.readAll();
+
+            // Writing the output to a file.
+            fileWriter.write(String.valueOf(list));
+        } catch(Exception e) {
+            System.out.println("CSV2JSON :: convertCSV2JSON :: Exception encountered converting to a JSON.");
+            e.printStackTrace();
+        }
+
+        System.out.println("CSV2JSON :: convertCSV2JSON :: Successfully converted " + sb.toString());
+        return sb.toString();
+
+    }
+
+}
