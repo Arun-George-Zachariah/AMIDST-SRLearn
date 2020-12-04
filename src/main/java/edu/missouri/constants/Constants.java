00961b2fcc725cb24664ac5c0c48f35ad7a5ee06
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-12-03 20:03:38 -0600

    Updating master string.

--- src/main/java/edu/missouri/constants/Constants.java
+++ src/main/java/edu/missouri/constants/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String JSON_EXTENSION = "json";
     public static final String DOT = ".";
     public static final String APP_NAME = "AMIDST-SRLearn";
-    public static final String MASTER = "localhost";
+    public static final String MASTER = "local";
 }
